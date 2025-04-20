69f1a2108a0d75846bcbe7fb7926826ed03d1851
ChesterBaltazar <chesterbaltazar13@gmail.com>
2025-04-20 19:55:36 +0800

    Add condition and show message

--- Test.cpp
+++ Test.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 int main(){
 	
-	double prices[3];
+	double prices[4];
 	//array values
 	prices[0] = 10.00;
 	prices[1] = 20.00;
 	prices[2] = 30.00;
-	
+	prices[3] = 40.00;
 	// array printed
-	if(prices <= 30){
-		cout << prices[0] << endl;
+	if(prices[2] <= 40){	
+		cout << prices[2] << " is less than or equal to " << 40 << endl;
 	}else{
-		cout << prices[1] << endl;
+		cout << prices[2] << " is greater than " << 40 << endl;
 	}
-
+	return 0;
 }
